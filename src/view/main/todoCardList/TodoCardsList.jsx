3bd56188bc3255529583f958c63bc3063fa623fe
HaeRheeNam <4ran0318@gmail.com>
2024-01-24 20:41:49 +0900

    [Refactor/#29] 피드백 반영

--- src/view/main/todoCardList/TodoCardsList.jsx
+++ src/view/main/todoCardList/TodoCardsList.jsx
@@ -1,16 +1,14 @@
-import React, { useState } from "react";
 import "./todoCardList.css";
 import TodoCards from "../todoCards/TodoCards";
 
 export const TodoCardsList = ({ todoInputs, setTodoInputs }) => {
-  // const isDoneState = todoInputs.filter((state) => state.isDone === true);
   const doneTrueTodos = todoInputs.filter((state) => state.isDone === true);
   const doneFalseTodos = todoInputs.filter((state) => state.isDone === false);
   return (
     <>
       <section>
         <div className="cards-wrap">
-          <h3>{doneTrueTodos ? "working" : "Done"} </h3>
+          <h3>Working</h3>
           <div className="todo-card-wrap">
             {doneTrueTodos.map((isDoneItem) => {
               const { id } = isDoneItem;
@@ -28,7 +26,7 @@ export const TodoCardsList = ({ todoInputs, setTodoInputs }) => {
       </section>
       <section>
         <div className="cards-wrap">
-          <h3>{!doneTrueTodos ? "working" : "Done"} </h3>
+          <h3>Done</h3>
           <div className="todo-card-wrap">
             {doneFalseTodos.map((isDoneItem) => {
               const { id } = isDoneItem;
