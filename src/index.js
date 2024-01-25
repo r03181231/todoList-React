677ecee084565d88bff73c85336d2c7269b005f2
HaeRheeNam <4ran0318@gmail.com>
2024-01-25 14:33:21 +0900

    [Refactor/#39] 첫번째 피드백 반영

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import MainPage from "view/MainPage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <MainPage />
   </React.StrictMode>
 );
 
